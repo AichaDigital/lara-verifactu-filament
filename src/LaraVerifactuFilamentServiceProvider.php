cd0ac1c3300e2bee4a45242d62aa9a6737ace39d
Abdelkarim Mateos Sanchez <abdelkarim.mateos@castris.com>
2025-12-25 13:42:10 +0100

    fix(deps): Remove incorrect larabill-filament dependency, add VCS repo
    - Remove larabill-filament from require (not used in this plugin)
    - Add VCS repository for lara-verifactu private package
    - Update lara-verifactu version constraint to ^0.2
    - Fix unused imports in ServiceProvider

--- src/LaraVerifactuFilamentServiceProvider.php
+++ src/LaraVerifactuFilamentServiceProvider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace AichaDigital\LaraVerifactuFilament;
 
-use AichaDigital\LaraVerifactuFilament\Commands\LaraVerifactuFilamentCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
